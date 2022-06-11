f96ba44fe095f7c5d51f716d0572e3fde76d2fc0
Tanvir Ahmed <tanvir15-9909@diu.edu.bd>
2022-06-12 04:26:13 +0600

    Email Password Authentication Done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import initializeAuthentication from "./Firebase/Firebase.initialize";
-import {getAuth, GoogleAuthProvider, signInWithPopup, signOut} from 'firebase/auth';
+import {getAuth, GoogleAuthProvider, signInWithPopup, signOut, createUserWithEmailAndPassword } from 'firebase/auth';
 import {useState} from 'react';
 import './App.css';
+
 
 initializeAuthentication();
 
@@ -9,9 +10,13 @@ const provider = new GoogleAuthProvider();
 
 function App() {
   const [user, setUser] = useState({});
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const auth = getAuth();
 
   const handleGoogleSignIn = () => {
-    const auth = getAuth();
+    
     signInWithPopup(auth, provider)
       .then(result => {
         const {email, displayName, photoURL} = result.user;
@@ -40,8 +45,58 @@ function App() {
       });
   }
 
+
+
+  const handleEmailChange = (event) => {
+    
+    setEmail(event.target.value);
+  }
+
+  const handlePasswordChange = (event) => {
+    
+    setPassword(event.target.value);
+  }
+
+  const handleCreateUser = (e) => {
+    e.preventDefault();
+    if(password.length < 6) {
+      setError('Password must be at least 6 characters long');
+      return;
+    }
+
+
+    
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(result => {
+        const user = result.user;
+        console.log(user);
+        })
+        
+        
+      }
+
   return (
-    <div className="App">
+    <div className="mx-5 p-5">
+      <form onSubmit={handleCreateUser}>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+          <input onBlur={handleEmailChange} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+          <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+          <input onBlur={handlePasswordChange} type="password" className="form-control" id="exampleInputPassword1"/>
+        </div>
+        <div className="mb-3 form-check">
+          <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+          <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+        </div>
+        <div className="mb-3 text-danger">{error}</div>
+        <button onClick={handleCreateUser} type="submit" className="btn btn-primary">Submit</button>
+      </form>
+
+      <div>===================================================</div>
+      <br /><br /><br />
       {
         !user.name ? 
         <button onClick={handleGoogleSignIn}>Click To Sign In</button>:
